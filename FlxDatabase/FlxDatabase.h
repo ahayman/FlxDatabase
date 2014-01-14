ba3db260102d0ee0ccc9d462c1134134248a76a7
Aaron Hayman <aaron@flexile.co>
2014-01-14 11:26:07 -0500

    Initial Commit

---
+++ FlxDatabase/FlxDatabase.h
@@ -0,0 +1,13 @@
+//
+//  FlxDatabase.h
+//  FlxDatabase
+//
+//  Created by Aaron Hayman on 1/14/14.
+//  Copyright (c) 2014 Aaron Hayman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlxDatabase : NSObject
+
+@end
---
+++ FlxDatabase/FlxDatabase.m
@@ -0,0 +1,13 @@
+//
+//  FlxDatabase.m
+//  FlxDatabase
+//
+//  Created by Aaron Hayman on 1/14/14.
+//  Copyright (c) 2014 Aaron Hayman. All rights reserved.
+//
+
+#import "FlxDatabase.h"
+
+@implementation FlxDatabase
+
+@end
